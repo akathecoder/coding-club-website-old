668ca1f75a41405207d8240da394793c5d7d42f9
Sparsh Agarwal <sparshaga25@gmail.com>
2021-01-17 09:46:22 +0530

    Implemented Direction, Image Overlay and Shadow in HomePage's Domains List

--- components/HomeDomain.js
+++ components/HomeDomain.js
@@ -7,39 +7,87 @@ function HomeDomain({
   link,
   right,
 }) {
-  return (
-    <div className="bg-gray-300 mx-80 mt-10 p-0 flex rounded-3xl">
-      <div className="m-0">
-        <Image
-          src="/assets/images/web-dev-1.jpg"
-          width="350"
-          height="350"
-          objectFit="cover"
-        />
-      </div>
-      <div className="mx-10 my-5 align-middle self-center">
-        <h1 className="text-3xl font-semibold ">
-          {title}
-        </h1>
-        <p className="text-lg break-words">
-          {description}
-        </p>
+  if (right) {
+    return (
+      <div className="bg-gray-200 mx-80 mt-44 p-0 flex rounded-3xl relative h-96 shadow-2xl ">
+        <div className="mx-10 my-5 align-middle self-center ml-16 ">
+          <h1 className="text-3xl font-semibold mb-8">
+            {title}
+          </h1>
+          <p className="text-lg break-words pr-24 ">
+            {description}
+          </p>
 
-        <button
-          type="submit"
-          className=" mx-2 my-6 px-10 py-4 bg-blue-500 rounded-full my-2 shadow-xl text-white font-semibold"
-        >
-          Visit
-        </button>
+          <a href={link}>
+            <button
+              type="submit"
+              className="  my-6 px-10 py-4 bg-blue-500 rounded-full  shadow-xl text-white font-semibold"
+            >
+              Visit
+            </button>
+          </a>
 
-        <button
-          type="submit"
-          className=" mx-6 my-6 px-6 py-4 bg-blue-500 rounded-full my-2 shadow-xl text-white font-semibold"
-        >
-          Projects
-        </button>
+          <a href={link + "/#projects"}>
+            <button
+              type="submit"
+              className=" mx-6 my-6 px-6 py-4 bg-blue-500 rounded-full shadow-xl text-white font-semibold"
+            >
+              Projects
+            </button>
+          </a>
+        </div>
+
+        <div className="transform scale-150 overflow-hidden">
+          <Image
+            className="rounded-2xl"
+            src="/assets/images/web-dev-1.jpg"
+            width="500"
+            height="500"
+            objectFit="cover"
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="bg-gray-200 mx-80 mt-44 p-0 flex rounded-3xl relative h-96 shadow-2xl">
+        <div className="transform scale-150 overflow-hidden">
+          <Image
+            className="rounded-2xl"
+            src="/assets/images/web-dev-1.jpg"
+            width="500"
+            height="500"
+            objectFit="cover"
+          />
+        </div>
+        <div className="mx-10 my-5 align-middle self-center ml-28">
+          <h1 className="text-3xl font-semibold mb-8">
+            {title}
+          </h1>
+          <p className="text-lg break-words">
+            {description}
+          </p>
+
+          <a href={link}>
+            <button
+              type="submit"
+              className=" mx-2 my-6 px-10 py-4 bg-blue-500 rounded-full  shadow-xl text-white font-semibold"
+            >
+              Visit
+            </button>
+          </a>
+
+          <a href={link + "/#projects"}>
+            <button
+              type="submit"
+              className=" mx-6 my-6 px-6 py-4 bg-blue-500 rounded-full shadow-xl text-white font-semibold"
+            >
+              Projects
+            </button>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 export default HomeDomain;
