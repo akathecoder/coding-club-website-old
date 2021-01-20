61e47226be22f001446eff4947e9082033b39d6e
Sparsh Agarwal <sparshaga25@gmail.com>
2021-01-20 17:30:38 +0530

    Fixed Navbar Social Icons Link to open on a blank

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,21 +16,30 @@ function Navbar() {
           S.P.A.R.K.S
         </h1>
         <div className="flex justify-end mx-2 md:mx-6 gap-2 md:gap-4 items-center">
-          <a href="https://www.instagram.com/sparks_jklu/">
+          <a
+            href="https://www.instagram.com/sparks_jklu/"
+            target="_blank"
+          >
             <FontAwesomeIcon
               icon={faInstagram}
               size="lg"
               color="black"
             />
           </a>
-          <a href="https://github.com/CodingClubJKLU">
+          <a
+            href="https://github.com/CodingClubJKLU"
+            target="_blank"
+          >
             <FontAwesomeIcon
               icon={faGithub}
               size="lg"
               color="black"
             />
           </a>
-          <a href="https://chat.whatsapp.com/DOPt14uV1H0EbXojJpB0Nf">
+          <a
+            href="https://chat.whatsapp.com/DOPt14uV1H0EbXojJpB0Nf"
+            target="_blank"
+          >
             <FontAwesomeIcon
               icon={faWhatsapp}
               size="lg"
