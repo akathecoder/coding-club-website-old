6f60d020f27c0f513ed1f1f743eb54d77760396d
Sparsh Agarwal <sparshaga25@gmail.com>
2021-01-20 21:42:44 +0530

    Added Image on Home Hero

--- components/HomePage/Hero.js
+++ components/HomePage/Hero.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import {
-  Link,
-  animateScroll as scroll,
-} from "react-scroll";
+import { Link } from "react-scroll";
+import Image from "next/image";
 
 function Hero() {
   return (
     <div className="md:py-32 py-10 bg-backgroundColor-main min-h-screen">
-      <div>
-        <h1 className="flex  md:justify-center md:text-center text-5xl px-6 md:text-6xl font-semibold md:leading-loose leading-relaxed ">
+      <div className="z-10">
+        <h1 className="flex md:justify-center md:text-center text-5xl px-6 md:text-6xl font-semibold md:leading-loose leading-relaxed">
           We are a community of <br />
           Programmers right here <br />
           at JKLU.
         </h1>
+      </div>
+
+      <div className="flex-initial sm:absolute sm:bottom-24   sm:right-40  transform sm:-rotate-6  p-3">
+        <Image
+          src="/assets/images/svg/undraw_code_thinking_1jeh.svg"
+          height="225"
+          width="541"
+        />{" "}
       </div>
 
       <div className="flex justify-center absolute w-3 mx-auto left-0 right-0 bottom-10 animate-bounce">
@@ -34,19 +40,6 @@ function Hero() {
           />
         </Link>
       </div>
-
-      {/* <div className="flex justify-center absolute w-3 mx-auto left-0 right-0 bottom-10 animate-bounce">
-        <a
-          className=" mx-6 px-4 py-5 bg-primaryColor-blue rounded-full my-2 shadow-xl "
-          href="/#domains"
-        >
-          <FontAwesomeIcon
-            icon={faArrowDown}
-            size="lg"
-            color="white"
-          />
-        </a>
-      </div> */}
     </div>
   );
 }
