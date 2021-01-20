bdc1fbbe2f216858465fb7dce424e37bf26c43bc
Sparsh Agarwal <sparshaga25@gmail.com>
2021-01-20 17:23:28 +0530

    Added SVG to 404

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 export default function FourOhFour() {
   return (
     <div class="flex h-screen bg-backgroundColor-main">
       <div class="m-auto align-middle">
-        <h1 className="text-2xl mb-4 align-middle">
+        {/* <h1 className="text-2xl mb-4 align-middle">
           <span className="text-4xl font-bold align-middle">
             404
           </span>
@@ -13,7 +14,16 @@ export default function FourOhFour() {
             |
           </span>
           This Page Could Not Be Found
-        </h1>
+        </h1> */}
+
+        <div className="p-10">
+          <Image
+            src="/assets/images/svg/undraw_page_not_found_su7k.svg"
+            height="500"
+            width="813"
+          />
+        </div>
+
         <div className="flex justify-center">
           <a
             className="px-5 py-4 mt-5 bg-blue-500 rounded-full shadow-xl  "
